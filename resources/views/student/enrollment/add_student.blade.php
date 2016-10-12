b8c33a7a8ecc71afee6a7d98e75970fcd5228fe3
udaragd <udaragd@gmail.com>
2016-10-12 22:03:57 +0530

    add student edit

--- resources/views/student/enrollment/add_student.blade.php
+++ resources/views/student/enrollment/add_student.blade.php
@@ -116,88 +116,38 @@
      			</div>
      			<div class="x_content">
         			<div class="col-md-4 col-sm-12 col-xs-12 form-group">
-            			<label class="control-label">INDEX NUMBER</label>
-             			<input type="text" class="form-control" id="tb_index_number" name="tb_index_number">
+            			<label class="control-label">NAME WITH INITIALS</label>
+             			<input type="text" class="form-control" id="tb_name_initials" name="tb_name_initials">
           			</div>
 
                     <div class="col-md-4 col-sm-12 col-xs-12 form-group">
-                        <label class="control-label">YEAR</label>
-                        <select class="form-control" id="year" name="year">
-                            <option value=''>SELECT YEAR</option>
-                        </select>
+                        <label class="control-label">FIRST NAME</label>
+                        <input type="text" class="form-control" id="tb_first_name" name="tb_first_name">
                     </div>
             
                     <div class="col-md-4 col-sm-12 col-xs-12 form-group">
-                        <label class="control-label">NIC</label>
-                        <input type="text" class="form-control" id="tb_nic" name="tb_nic">
+                        <label class="control-label">MIDDLE NAME</label>
+                        <input type="text" class="form-control" id="tb_middle_name" name="tb_middle_name">
                     </div>
             
                     <div class="col-md-4 col-sm-12 col-xs-12 form-group">
-                        <label class="control-label">DOB YEAR</label>
-                        <select class="form-control" id="sb_year" name="sb_year">
-                            <option value=''>SELECT YEAR</option>
-                        </select>
+                        <label class="control-label">LAST NAME</label>
+                        <input type="text" class="form-control" id="tb_last_name" name="tb_last_name">
                     </div>
                     
                     <div class="col-md-4 col-sm-12 col-xs-12 form-group">
-                        <label class="control-label">DOB MONTH</label>
-                        <select class="form-control" id="sb_month" name="sb_month">
-                            <option value=''>SELECT MONTH</option>
-                            <option value='1'>JANAURY</option>
-                            <option value='2'>FEBRUARY</option>
-                            <option value='3'>MARCH</option>
-                            <option value='4'>APRIL</option>
-                            <option value='5'>MAY</option>
-                            <option value='6'>JUNE</option>
-                            <option value='7'>JULY</option>
-                            <option value='8'>AUGUST</option>
-                            <option value='9'>SEPTEMBER</option>
-                            <option value='10'>OCTOBER</option>
-                            <option value='11'>NOVEMBER</option>
-                            <option value='12'>DECEMBER</option>
-                        </select> 
+                        <label class="control-label">ADDRESS : LINE 1</label>
+                        <input type="text" class="form-control" id="tb_address_l1" name="tb_address_l1">
                     </div>
             
                     <div class="col-md-4 col-sm-12 col-xs-12 form-group">
-                        <label class="control-label">DOB DAY</label>
-                        <select class="form-control" id="sb_day" name="sb_day">
-                            <option value=''>SELECT DAYS</option>
-                            <?php for($i=1;$i<=31;$i++){ ?>
-                                <option value='<?php echo $i; ?>'><?php echo $i; ?></option>
-                            <?php }?>
-                        </select>
+                        <label class="control-label">ADDRESS : LINE 2</label>
+                        <input type="text" class="form-control" id="tb_address_l2" name="tb_address_l2">
                     </div>
                     
                     <div class="col-md-4 col-sm-12 col-xs-12 form-group">
-                        <label class="control-label">GENDER</label>
-                        <select class="form-control" id="sb_gender" name="sb_gender">
-                            <option value=''>SELECT GENDER</option>
-                            <option value='male'>MALE</option>
-                            <option value='female'>FEMALE</option>
-                        </select>
-                    </div>
-            
-                    <div class="col-md-4 col-sm-12 col-xs-12 form-group">
-                        <label class="control-label">CONTACT : HOME</label>
-                        <input type="text" class="form-control" id="tb_hm_no" name="tb_hm_no">
-                    </div>
-                    
-                    <div class="col-md-4 col-sm-12 col-xs-12 form-group">
-                        <label class="control-label">CONTACT : MOBILE</label>
-                        <input type="text" class="form-control" id="tb_mobile" name="tb_mobile">
-                    </div>
-                    
-                    <div class="col-md-4 col-sm-12 col-xs-12 form-group">
-                        <label class="control-label">MEDIUM</label>
-                        <select class="form-control" id="sb_medium" name="sb_medium">
-                            <option value='0'>SELECT MEDIUM</option>
-                            <?php echo getMedium("") ?>
-                        </select>
-                    </div>
-            
-                    <div class="col-md-4 col-sm-12 col-xs-12 form-group">
-                        <label class="control-label">EMAIL</label>
-                        <input type="email" class="form-control" id="tb_email" name="tb_email">
+                        <label class="control-label">ADDRESS : LINE 3</label>
+                        <input type="text" class="form-control" id="tb_address_l3" name="tb_address_l3">
                     </div>
                     
                     <div class="form-group">
